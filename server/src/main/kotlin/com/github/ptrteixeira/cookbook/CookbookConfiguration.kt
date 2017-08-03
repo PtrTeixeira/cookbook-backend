98b75f7934f9fa1930c8a10d088d315fa9ec87c7
Peter Teixeira <PtrTeixeira@gmail.com>
2017-08-03 16:32:45 -0400

    Add programmatic support for running in Docker
    Two important changes here:
     1. Configuration is now really stored in environment variables.
        Those environment variables get substituted into the config file,
        which the application reads.
     2. The Liquibase migration can be run configured to run progammatically
        when the app starts. This should be off when running it against
        a permanent database, but should be on if you're dumb and doing what
        I'm doing at the moment, which is keeping the data file in the
        container.
    
    In the future, the config file will probably be a list of contexts to
    be run when the application starts, which makes more sense.

--- server/src/main/kotlin/com/github/ptrteixeira/cookbook/CookbookConfiguration.kt
+++ server/src/main/kotlin/com/github/ptrteixeira/cookbook/CookbookConfiguration.kt
@@ -7,5 +7,6 @@ import io.dropwizard.db.DataSourceFactory
 class CookbookConfiguration(
     val database: DataSourceFactory = DataSourceFactory(),
     val oauthConfig: OauthConfiguration,
+    val autoRunMigration: Boolean = false,
     val baseUrl: String = "http://localhost:8080"
 ) : Configuration()
