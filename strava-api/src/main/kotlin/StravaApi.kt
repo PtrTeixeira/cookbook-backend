ea5050928c68d8a65469eeb42e6542435afa8402
Peter Teixeira <ptrTeixeira@gmail.com>
2018-06-16 15:37:36 -0400

    Add method to get auth token for user

--- strava-api/src/main/kotlin/StravaApi.kt
+++ strava-api/src/main/kotlin/StravaApi.kt
@@ -1,9 +1,11 @@
 package com.github.ptrteixeira.strava.api
 
 import com.github.ptrteixeira.strava.api.models.AthleteActivitiesResponse
+import com.github.ptrteixeira.strava.api.models.TokenResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface StravaApi {
@@ -30,4 +32,11 @@ interface StravaApi {
             @Query("page") page: Int = 1,
             @Query("per_page") itemsPerPage: Int = 30
     ): Single<List<AthleteActivitiesResponse>>
+
+    @POST
+    fun getAuthToken(
+            @Query("client_id") clientId: String,
+            @Query("client_secret") clientSecret: String,
+            @Query("code") code: String
+    ): Single<TokenResponse>
 }
