64d07e4131d3d69120abf0c4f7c7165edf95c27c
Peter Teixeira <PtrTeixeira@gmail.com>
2019-02-28 12:40:09 -0500

    Finish move to bazel

--- punchcard/ui/build.sh
+++ punchcard/ui/build.sh
@@ -2,5 +2,5 @@
 set -eo pipefail
 
 # Workaround until I learn how to use bazel
-bazel run //bzl/parcel -- build $PWD/index.html --out-dir $PWD/dist
+bazel run //bzl/parcel -- build $PWD/public/index.html --out-dir $PWD/dist
 
