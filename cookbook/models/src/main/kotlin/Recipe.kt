effd83b1d348a2368e7254f3ed6607f3fd27db75
Peter Teixeira <PtrTeixeira@gmail.com>
2019-03-01 19:39:09 -0500

    --wip-- [skip ci]

---
+++ cookbook/models/src/main/kotlin/Recipe.kt
@@ -0,0 +1,21 @@
+package com.github.ptrteixeira.cookbook.core
+
+/**
+ * Represents a Recipe in the domain com.github.ptrteixeira.cookbook.model of this project.
+ *
+ * @property [id] Identifier in the database for this recipe
+ * @property [name] Name of this recipe when displayed
+ * @property [ingredients] What goes into the recipe?
+ * @property [instructions] How do you make this? Interpreted more in paragraph style than step style
+ * @property [summary] Long description of the recipe
+ * @property [description] Short description of the recipe
+ */
+data class Recipe(
+    val id: Int,
+    val userId: String,
+    val name: String = "",
+    val ingredients: List<String> = listOf(),
+    val instructions: String = "",
+    val summary: String = "",
+    val description: String = ""
+)
---
+++ cookbook/models/src/main/kotlin/User.kt
@@ -0,0 +1,7 @@
+package com.github.ptrteixeira.cookbook.core
+
+import java.security.Principal
+
+data class User(val id: String) : Principal {
+    override fun getName(): String = id
+}
