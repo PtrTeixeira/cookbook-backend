4373530c5f8879938185f91653d164078efd0c69
Peter Teixeira <PtrTeixeira@gmail.com>
2019-03-05 13:42:03 -0500

    Get recipe server to build

--- cookbook/server/src/main/kotlin/CookbookApplication.kt
+++ cookbook/server/src/main/kotlin/CookbookApplication.kt
@@ -4,28 +4,23 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.github.ptrteixeira.cookbook.base.BaseModule
 import com.github.ptrteixeira.cookbook.core.User
 import com.github.ptrteixeira.cookbook.config.CookbookConfiguration
+import com.github.ptrteixeira.cookbook.data.RecipeData
 import com.github.ptrteixeira.cookbook.data.migrationsBundle
+import com.github.ptrteixeira.cookbook.resources.RecipesResource
 import com.github.ptrteixeira.dropwizard.support.configure
 import com.tylerkindy.dropwizard.dagger.DaggerApplication
 import com.tylerkindy.dropwizard.dagger.DropwizardInjector
+import io.dropwizard.Application
 import io.dropwizard.auth.Authenticator
+import io.dropwizard.jdbi3.JdbiFactory
 import io.dropwizard.jdbi3.bundles.JdbiExceptionsBundle
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
 import javax.inject.Inject
 
-class CookbookApplication : DaggerApplication<CookbookConfiguration>() {
+class CookbookApplication : Application<CookbookConfiguration>() {
     @Inject
     internal lateinit var authenticator: Authenticator<String, User>
-
-    override fun applicationInjector(
-        configuration: CookbookConfiguration,
-        environment: Environment
-    ): DropwizardInjector<out DaggerApplication<*>> {
-        return DaggerApplicationComponent.builder()
-                .baseModule(BaseModule(configuration, environment))
-                .build()
-    }
 
     override fun initialize(bootstrap: Bootstrap<CookbookConfiguration>?) {
         configure(bootstrap) {
@@ -42,12 +37,21 @@ class CookbookApplication : DaggerApplication<CookbookConfiguration>() {
     }
 
     override fun run(configuration: CookbookConfiguration, environment: Environment) {
-        super.run(configuration, environment)
+        val database = configuration.database
+        val factory = JdbiFactory()
+            .build(environment, database, "h2")
+            .installPlugins()
+            .registerArrayType(String::class.java, "varchar")
+        val recipeData = factory.onDemand(RecipeData::class.java)
+        val recipesResource = RecipesResource(recipeData)
 
         configure(environment) {
             oauthFilter<User> {
                 setAuthenticator(authenticator)
             }
+
+
+            resources(recipesResource)
         }
     }
 
