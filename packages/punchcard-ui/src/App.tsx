fb2b5bceb7c7f7047204d04becd104b429297873
Peter Teixeira <PtrTeixeira@gmail.com>
2019-03-12 14:20:32 -0400

    Add Bootstrap

--- packages/punchcard-ui/src/App.tsx
+++ packages/punchcard-ui/src/App.tsx
@@ -23,33 +23,31 @@ function withProps(component: JSX.Element) {
 
 function HomeRoute() {
   return (
-  <Route exact={true} path="/" render={withProps(
-     <a href="/api/strava/login">
-       <img src={loginButton} alt="Connect with Strava" className="home-connect-btn" />
-     </a>
-  )} />)
+    <Route exact={true} path="/" render={withProps(
+      <a href="/api/strava/login">
+        <img src={loginButton} alt="Connect with Strava" className="home-connect-btn" />
+      </a>
+    )} />)
 }
 
 function WeekMapRoute(props: IAppState) {
   return (
-  <Route path="/dashboard" render={withProps(
-    <Dashboard {...props}/>
-  )} />)
+    <Route path="/dashboard" render={withProps(
+      <Dashboard {...props} />
+    )} />)
 }
 
 
-function loadWeeklyResults(): Promise<IWeeklyResults> {
+async function loadWeeklyResults(): Promise<IWeeklyResults> {
   const storageContents = sessionStorage.getItem("results")
   if (storageContents != null) {
     return Promise.resolve(JSON.parse(storageContents))
   } else {
-    return axios
-      .get("/api/punchcard")
-      .then((response: AxiosResponse<IWeeklyResults>) => response.data)
-      .then((data: IWeeklyResults) => {
-        sessionStorage.setItem("results", JSON.stringify(data))
-        return data
-      });
+    const response = await axios
+      .get("/api/punchcard");
+    const data = response.data;
+    sessionStorage.setItem("results", JSON.stringify(data));
+    return data;
   }
 }
 
@@ -91,15 +89,15 @@ class App extends React.Component<{}, IAppState> {
   public render() {
     return (
       <Router>
-        <div className="App">
-            <Header />
+        <div className="App container-fluid">
+          <Header />
 
-            <HomeRoute />
-            <WeekMapRoute {...this.state} />
-            <footer>
-              <img src={stravaLogo} alt="Powered by Strava" className="app-strava-logo" />
-            </footer>
-          </div>
+          <HomeRoute />
+          <WeekMapRoute {...this.state} />
+          <footer>
+            <img src={stravaLogo} alt="Powered by Strava" className="app-strava-logo" />
+          </footer>
+        </div>
       </Router>
     )
   }
--- packages/punchcard-ui/src/header/Header.tsx
+++ packages/punchcard-ui/src/header/Header.tsx
@@ -6,7 +6,6 @@ import logo from '../logo.svg'
 export function Header() {
     return (
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Your Running Punchcard</h1>
         </header>
     )
