41608c3a3494d8a7d0fcb22bf8ecefd7331cffc9
Peter Teixeira <PtrTeixeira@gmail.com>
2019-10-26 11:12:31 -0400

    Add an integer assertion

--- go-assert/assert.go
+++ go-assert/assert.go
@@ -1,22 +1,47 @@
 package assert
 
 import (
+	"fmt"
 	"testing"
 )
 
 type BooleanAssertion func(val bool) (bool, string)
+type IntegerAssertion func(val int) (bool, string)
 
 func IsTrue() BooleanAssertion {
-	return func(val bool) (bool, string) {
-    if !val {
-      return false, "Expected true but was: \"false\""
-    }
-    return val, ""
-  }
+	return IsEqualToBool(true)
 }
 
-func AssertThat(t *testing.T, val bool, test BooleanAssertion) {
-  if result, msg := test(val); !result {
-    t.Error(msg)
-  }
+func IsFalse() BooleanAssertion {
+	return IsEqualToBool(false)
+}
+
+func IsEqualToBool(cond bool) BooleanAssertion {
+	return func(val bool) (bool, string) {
+		if val != cond {
+			return false, fmt.Sprintf("Expected \"%v\" but was: \"%v\"", cond, val)
+		}
+		return true, ""
+	}
+}
+
+func IsEqualToInt(cond int) IntegerAssertion {
+	return func(val int) (bool, string) {
+		if val != cond {
+			return false, fmt.Sprintf("Expected \"%v\" but was: \"%v\"", cond, val)
+		}
+		return true, ""
+	}
+}
+
+func AssertThatBool(t *testing.T, val bool, test BooleanAssertion) {
+	if result, msg := test(val); !result {
+		t.Error(msg)
+	}
+}
+
+func AssertThatInt(t *testing.T, val int, test IntegerAssertion) {
+	if result, msg := test(val); !result {
+		t.Error(msg)
+	}
 }
--- go-assert/assert_test.go
+++ go-assert/assert_test.go
@@ -5,33 +5,33 @@ import (
 )
 
 func TestBooleanAssertion(t *testing.T) {
-  t.Run("IsTrue matches true", func(t *testing.T) {
-    matcher := IsTrue()
-    result, _ := matcher(true)
+	t.Run("IsTrue matches true", func(t *testing.T) {
+		matcher := IsTrue()
+		result, _ := matcher(true)
 
-    if !result {
-      t.Errorf("Should have matched true!")
-    }
-  })
+		if !result {
+			t.Errorf("Should have matched true!")
+		}
+	})
 
-  t.Run("IsTrue does not match false", func(t *testing.T) {
-    matcher := IsTrue()
-    result, msg := matcher(false)
+	t.Run("IsTrue does not match false", func(t *testing.T) {
+		matcher := IsTrue()
+		result, msg := matcher(false)
 
-    if result {
-      t.Errorf("Should not have matched false!")
-    }
-    if msg != "Expected true but was: \"false\"" {
-      t.Errorf("Got incorrect message: %q", msg)
-    }
-  })
+		if result {
+			t.Errorf("Should not have matched false!")
+		}
+		if msg != "Expected \"true\" but was: \"false\"" {
+			t.Errorf("Got incorrect message: %q", msg)
+		}
+	})
 }
 
-// TODO(pteixeira) Need to modify how the stackframe unwinds 
-// to get the error message to come from lines like this, 
+// TODO(pteixeira) Need to modify how the stackframe unwinds
+// to get the error message to come from lines like this,
 // rather than from a line in this library
 func TestAssertThat(t *testing.T) {
-  t.Run("AssertThat does nothing when matched", func(t *testing.T) {
-    AssertThat(t, true, IsTrue())
-  })
+	t.Run("AssertThat does nothing when matched", func(t *testing.T) {
+		AssertThatBool(t, true, IsTrue())
+	})
 }
