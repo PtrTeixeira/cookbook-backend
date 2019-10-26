2a847c5f677f9c5959ce2bfada6376fe304ea6b7
Peter Teixeira <PtrTeixeira@gmail.com>
2019-10-26 11:20:13 -0400

    Use own assert library here
    Use my own tiny assertions library here. I kinda like the way it looks,
    ngl, and with Bazel this is way, way easier than trying to use external
    deps in libraries. So this is how we're gonna do this.

--- punchcard/go-server/stats_test.go
+++ punchcard/go-server/stats_test.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"testing"
+
+	. "github.com/PtrTeixeira/cookbook/go-assert"
 )
 
 func TestAddToMap(t *testing.T) {
 	t.Run("it sets the value to 1 if the value was absent", func(t *testing.T) {
 		dest := make(map[int]int)
 		increment(4, dest)
-		if dest[4] != 1 {
-			t.Errorf("Expected value 1, was %v", dest[4])
-		}
+		AssertThatInt(t, dest[4], IsEqualToInt(1))
 	})
+
 	t.Run("it increments the value in the map if it is present", func(t *testing.T) {
 		dest := make(map[int]int)
 		dest[4] = 2
 		increment(4, dest)
-		if dest[4] != 3 {
-			t.Errorf("Expected value 3, was %v", dest[4])
-		}
+		AssertThatInt(t, dest[4], IsEqualToInt(3))
 	})
 }
