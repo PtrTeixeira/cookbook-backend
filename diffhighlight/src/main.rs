347b66c2ca50c20e0b4dc45d6c1d76b75278ad13
Peter Teixeira <PtrTeixeira@gmail.com>
2019-11-24 22:39:15 -0500

    Halfway to highlighting

--- diffhighlight/src/main.rs
+++ diffhighlight/src/main.rs
@@ -6,19 +6,20 @@ use std::io::{self, BufRead};
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let mut in_hunk = false;
+    let mut added_lines = Vec::new();
+    let mut removed_lines = Vec::new();
+
     for line in stdin.lock().lines() {
         let content = line.unwrap();
         if !in_hunk {
             println!("{}", content);
             in_hunk = is_start_of_hunk(&content);
-            if in_hunk {
-                println!("Entering hunk")
-            }
         } else if is_line_added(&content) {
-            println!("Line added");
+            added_lines.push(content);
         } else if is_line_removed(&content) {
-            println!("Line removed");
+            removed_lines.push(content);
         } else {
+            flush(&mut added_lines, &mut removed_lines);
             println!("{}", content);
             in_hunk = does_hunk_continue(&content);
             if !in_hunk {
@@ -27,6 +28,10 @@ fn main() -> io::Result<()> {
         }
     }
 
+    if in_hunk {
+        flush(&mut added_lines, &mut removed_lines);
+        println!("Exiting after hunk")
+    }
     Ok(())
 }
 
@@ -48,4 +53,32 @@ fn is_line_removed(line: &str) -> bool {
 fn does_hunk_continue(line: &str) -> bool {
     let hunk_marker = Regex::new(r"^[@ ]").unwrap();
     hunk_marker.is_match(line)
+}
+
+fn flush(added: &mut Vec<String>, removed: &mut Vec<String>) -> () {
+    if added.len() == 0 && removed.len() == 0 {
+        return
+    } else if added.len() == removed.len() {
+        println!("Changed: {}", added.len())
+    } else {
+        print_added(added);
+        print_removed(removed);
+    }
+
+    added.clear();
+    removed.clear();
+}
+
+fn print_added(lines: &Vec<String>) -> () {
+    let green = "\x1b[32;1m";
+    let reset = "\x1b[0m";
+    for line in lines {
+        println!("{}{}{}", green, line, reset)
+    }
+}
+
+fn print_removed(lines: &Vec<String>) -> () {
+    for line in lines {
+        println!("{}", line)
+    }
 }
